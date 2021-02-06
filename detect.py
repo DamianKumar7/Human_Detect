6243e117cde5882e9a7e05d322eebe8063ac7259
DamianKumar7 <59192428+DamianKumar7@users.noreply.github.com>
2021-02-06 23:10:07 +0530

    1st commit

---
+++ detect.py
@@ -0,0 +1,75 @@
+import numpy as np
+import cv2
+
+'''hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
+
+# open webcam video stream
+cap = cv2.VideoCapture(0)
+
+# the output will be written to output.avi
+out = cv2.VideoWriter(
+    'output.avi',
+    cv2.VideoWriter_fourcc(*'MJPG'),
+    15.,
+    (640, 480))
+
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # resizing for faster detection
+    frame = cv2.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                      (0, 255, 0), 2)
+
+    # Write the output video
+    out.write(frame.astype('uint8'))
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+# and release the output
+out.release()
+# finally, close the window
+cv2.destroyAllWindows()
+cv2.waitKey(1)'''
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+video = cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    faces = face_cascade.detectMultiScale(frame,
+                                          scaleFactor=1.1, minNeighbors=5)
+    for x,y,w,h in faces:
+        frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+
+    cv2.imshow('Face Detector', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
